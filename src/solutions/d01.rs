d9767a9b8b87547e379ed2ef3126ab358e709e59
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-23 17:54:30 +0100

    Optimize d01a from 151272us to 11190us.

--- src/solutions/d01.rs
+++ src/solutions/d01.rs
@@ -3,22 +3,39 @@ use alloc::vec::Vec;
 use itertools::Itertools;
 use rtt_target::rprintln;
 
+/// Measured speed: 11190us.
 pub fn p1(input: Vec<u8>) {
-    rprintln!(
-        "d01a: {}",
-        input
-            .into_iter()
-            .map(|d| d as char)
-            .collect::<String>()
-            .split("\n\n")
-            .map(|elf| {
-                elf.lines()
-                    .map(|calories| calories.parse::<u32>().unwrap())
-                    .sum::<u32>()
-            })
-            .max()
-            .unwrap()
-    );
+    // Initialize variables to keep track of maximum sum, current group sum and current line sum
+    let mut max_sum = 0;
+    let mut current_group_sum = 0;
+    let mut current_line_sum = 0;
+
+    // Initialize variable to keep track of whether the last byte encountered was a newline
+    let mut last_byte_was_newline = false;
+
+    for byte in input {
+        match (byte, last_byte_was_newline) {
+            // If the current byte is a newline and the last byte was also a newline
+            (b'\n', true) => {
+                max_sum = max_sum.max(current_group_sum);
+                current_group_sum = 0;
+                current_line_sum = 0;
+                last_byte_was_newline = false;
+            }
+            // If the current byte is a newline and the last byte wasn't a newline
+            (b'\n', _) => {
+                current_group_sum += current_line_sum;
+                current_line_sum = 0;
+                last_byte_was_newline = true;
+            }
+            // Otherwise the current byte is a digit
+            (_, _) => {
+                current_line_sum = current_line_sum * 10 + (byte - b'0') as u32;
+                last_byte_was_newline = false;
+            }
+        }
+    }
+    rprintln!("d01a: {}", max_sum);
 }
 
 pub fn p2(input: Vec<u8>) {
