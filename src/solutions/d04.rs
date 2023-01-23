4b727b809ea6c4adb81d28ae5e36cc5ae6611575
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-23 23:23:19 +0100

    Optimize d04a and d04b from around 200000us to respectively 20110us and 20433us.

--- src/solutions/d04.rs
+++ src/solutions/d04.rs
@@ -1,47 +1,60 @@
-use alloc::string::String;
 use alloc::vec::Vec;
 use rtt_target::rprintln;
 
+/// Measured speed: 20110us.
 pub fn p1(input: Vec<u8>) {
-    rprintln!(
-        "d04a: {}",
-        input
-            .into_iter()
-            .map(|d| d as char)
-            .collect::<String>()
-            .lines()
-            .filter(|pair| {
-                let (first, second) = pair.split_once(',').unwrap();
-                let (a, b) = first.split_once('-').unwrap();
-                let (c, d) = second.split_once('-').unwrap();
-                let a = a.parse::<usize>().unwrap();
-                let b = b.parse::<usize>().unwrap();
-                let c = c.parse::<usize>().unwrap();
-                let d = d.parse::<usize>().unwrap();
-                (a >= c && b <= d) || (a <= c && b >= d)
-            })
-            .count()
-    );
+    let (mut a, mut b, mut c, mut d) = (0, 0, 0, 0);
+    let mut counter = 0;
+    let mut total = 0;
+    for byte in input {
+        match byte {
+            b'-' | b',' => counter += 1,
+            b'\n' => {
+                if (a >= c && b <= d) || (a <= c && b >= d) {
+                    total += 1;
+                }
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                counter = 0;
+            }
+            _ => match counter {
+                0 => a = a * 10 + (byte - b'0'),
+                1 => b = b * 10 + (byte - b'0'),
+                2 => c = c * 10 + (byte - b'0'),
+                _ => d = d * 10 + (byte - b'0'),
+            },
+        }
+    }
+    rprintln!("d04a: {}", total);
 }
 
+/// Measured speed: 20433us
 pub fn p2(input: Vec<u8>) {
-    rprintln!(
-        "d04b: {}",
-        input
-            .into_iter()
-            .map(|d| d as char)
-            .collect::<String>()
-            .lines()
-            .filter(|pair| {
-                let (first, second) = pair.split_once(',').unwrap();
-                let (a, b) = first.split_once('-').unwrap();
-                let (c, d) = second.split_once('-').unwrap();
-                let a = a.parse::<usize>().unwrap();
-                let b = b.parse::<usize>().unwrap();
-                let c = c.parse::<usize>().unwrap();
-                let d = d.parse::<usize>().unwrap();
-                a <= d && c <= b
-            })
-            .count()
-    );
+    let (mut a, mut b, mut c, mut d) = (0, 0, 0, 0);
+    let mut counter = 0;
+    let mut total = 0;
+    for byte in input {
+        match byte {
+            b'-' | b',' => counter += 1,
+            b'\n' => {
+                if a <= d && c <= b {
+                    total += 1;
+                }
+                a = 0;
+                b = 0;
+                c = 0;
+                d = 0;
+                counter = 0;
+            }
+            _ => match counter {
+                0 => a = a * 10 + (byte - b'0'),
+                1 => b = b * 10 + (byte - b'0'),
+                2 => c = c * 10 + (byte - b'0'),
+                _ => d = d * 10 + (byte - b'0'),
+            },
+        }
+    }
+    rprintln!("d04b: {}", total);
 }
