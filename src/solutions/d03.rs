8eb33c979804eab019d59fe618da2a28e97e3869
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-23 19:41:12 +0100

    Optimize d03a from 96474us down to 43428us.

--- src/solutions/d03.rs
+++ src/solutions/d03.rs
@@ -3,25 +3,26 @@ use alloc::vec::Vec;
 use itertools::Itertools;
 use rtt_target::rprintln;
 
+/// Measured speed: 43428us.
 pub fn p1(input: Vec<u8>) {
-    rprintln!(
-        "d03a: {}",
-        input
-            .into_iter()
-            .map(|d| d as char)
-            .collect::<String>()
-            .lines()
-            .map(|rucksack| {
-                let (first, second) = rucksack.split_at(rucksack.len() / 2);
-                first.chars().find(|&item| second.contains(item)).unwrap()
-            })
-            .map(|misplaced_item| match misplaced_item {
-                'a'..='z' => misplaced_item as usize - b'a' as usize + 1,
-                'A'..='Z' => misplaced_item as usize - b'A' as usize + 27,
-                _ => unreachable!(),
-            })
-            .sum::<usize>()
-    );
+    let mut sum = 0;
+    for rucksack in input.split(|&byte| byte == b'\n') {
+        let first_half = &rucksack[0..rucksack.len() / 2];
+        let second_half = &rucksack[rucksack.len() / 2..rucksack.len()];
+        for item in first_half {
+            if second_half.contains(item) {
+                // Check if the byte is a lowercase or uppercase letter
+                if *item >= b'a' {
+                    sum += (item - (b'a' - 1)) as u32;
+                } else {
+                    sum += (item - (b'A' - 27)) as u32;
+                }
+                // Break out of the loop, as we only need the first matching byte
+                break;
+            }
+        }
+    }
+    rprintln!("d03a: {}", sum);
 }
 
 pub fn p2(input: Vec<u8>) {
