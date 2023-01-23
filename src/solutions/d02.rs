6e23126adfc76f0308d54a1ad8e04f4944636eb2
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-23 18:41:15 +0100

    Optimize d02a and d02b down from 187970us to 14777us.

--- src/solutions/d02.rs
+++ src/solutions/d02.rs
@@ -1,51 +1,64 @@
-use alloc::string::String;
 use alloc::vec::Vec;
 use rtt_target::rprintln;
 
+/// Measured speed: 14777us.
 pub fn p1(input: Vec<u8>) {
-    rprintln!(
-        "d02a: {}",
-        input
-            .into_iter()
-            .map(|d| d as char)
-            .collect::<String>()
-            .lines()
-            .map(|game| match game {
-                "A X" => 1 + 3,
-                "A Y" => 2 + 6,
-                "A Z" => 3,
-                "B X" => 1,
-                "B Y" => 2 + 3,
-                "B Z" => 3 + 6,
-                "C X" => 1 + 6,
-                "C Y" => 2,
-                "C Z" => 3 + 3,
-                _ => unreachable!(),
-            })
-            .sum::<usize>()
-    );
+    // Initialize variable to keep track of the total score
+    let mut total_score = 0u32;
+
+    // Initialize variable to keep track of the last played shape by opponent
+    let mut opponent = b'\n';
+
+    for byte in input {
+        match (opponent, byte) {
+            // If byte is one of [b'A', b'B', b'C'] then this is opponent's shape
+            (_, b'A') => opponent = b'A',
+            (_, b'B') => opponent = b'B',
+            (_, b'C') => opponent = b'C',
+            // If byte is one of [b'X', b'Y', b'Z'] then add the score of the game to the total
+            (b'A', b'X') => total_score += 1 + 3,
+            (b'A', b'Y') => total_score += 2 + 6,
+            (b'A', b'Z') => total_score += 3,
+            (b'B', b'X') => total_score += 1,
+            (b'B', b'Y') => total_score += 2 + 3,
+            (b'B', b'Z') => total_score += 3 + 6,
+            (b'C', b'X') => total_score += 1 + 6,
+            (b'C', b'Y') => total_score += 2,
+            (b'C', b'Z') => total_score += 3 + 3,
+            // Otherwise byte is not a shape
+            _ => {}
+        }
+    }
+    rprintln!("d02a: {}", total_score);
 }
 
+/// Measured speed: 14777us.
 pub fn p2(input: Vec<u8>) {
-    rprintln!(
-        "d02b: {}",
-        input
-            .into_iter()
-            .map(|d| d as char)
-            .collect::<String>()
-            .lines()
-            .map(|game| match game {
-                "A X" => 3,
-                "A Y" => 1 + 3,
-                "A Z" => 2 + 6,
-                "B X" => 1,
-                "B Y" => 2 + 3,
-                "B Z" => 3 + 6,
-                "C X" => 2,
-                "C Y" => 3 + 3,
-                "C Z" => 1 + 6,
-                _ => unreachable!(),
-            })
-            .sum::<usize>()
-    );
+    // Initialize variable to keep track of the total score
+    let mut total_score = 0u32;
+
+    // Initialize variable to keep track of the last played shape by opponent
+    let mut opponent = b'\n';
+
+    for byte in input {
+        match (opponent, byte) {
+            // If byte is one of [b'A', b'B', b'C'] then this is opponent's shape
+            (_, b'A') => opponent = b'A',
+            (_, b'B') => opponent = b'B',
+            (_, b'C') => opponent = b'C',
+            // If byte is one of [b'X', b'Y', b'Z'] then add the score of the game to the total
+            (b'A', b'X') => total_score += 3,
+            (b'A', b'Y') => total_score += 1 + 3,
+            (b'A', b'Z') => total_score += 2 + 6,
+            (b'B', b'X') => total_score += 1,
+            (b'B', b'Y') => total_score += 2 + 3,
+            (b'B', b'Z') => total_score += 3 + 6,
+            (b'C', b'X') => total_score += 2,
+            (b'C', b'Y') => total_score += 3 + 3,
+            (b'C', b'Z') => total_score += 1 + 6,
+            // Otherwise byte is not a shape
+            _ => {}
+        }
+    }
+    rprintln!("d0ba: {}", total_score);
 }
