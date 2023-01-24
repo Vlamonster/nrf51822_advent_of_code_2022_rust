c1a01d967b4bf8d56256f2da53882d1daf37ed42
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-24 05:29:56 +0100

    Optimize d07b down to 28722us.

--- src/solutions/d07.rs
+++ src/solutions/d07.rs
@@ -43,40 +43,38 @@ pub fn p1(input: Vec<u8>) {
     )
 }
 
+/// Measured speed: 28722us.
 pub fn p2(input: Vec<u8>) {
-    let mut directories = BTreeMap::<Vec<_>, u32>::new();
-    let root: Vec<&[u8]> = vec![b"/"];
-    let mut path = Vec::new();
+    let mut directories = vec![0];
+    let mut path = vec![0];
     for line in input.split(|&d| d == b'\n') {
         match line {
-            b"$ cd /" => {
-                path = root.clone();
-                directories.insert(path.clone(), 0);
-            }
             b"$ cd .." => {
-                path.pop();
+                let inner = directories[path.pop().unwrap()];
+                directories[*path.last().unwrap()] += inner;
             }
-            _ if line.starts_with(b"$ cd ") => {
-                let directory = &line[5..];
-                path.push(directory);
-                directories.insert(path.clone(), 0);
-            }
-            b"$ ls" | b"" => {}
+            b"$ cd /" | b"" | b"$ ls" => {}
             _ if line.starts_with(b"dir") => {}
-            _ => {
-                let file_size = line
-                    .split(|&d| d == b' ')
-                    .next()
-                    .unwrap()
-                    .iter()
-                    .map(|&d| d as char)
-                    .collect::<String>()
-                    .parse::<u32>()
-                    .unwrap();
-                for i in 1..=path.len() {
-                    directories.get_mut(&path[..i]).map(|v| *v += file_size);
-                }
+            _ if line.starts_with(b"$ cd ") => {
+                path.push(directories.len());
+                directories.push(0);
             }
+            _ => {
+                let mut file_size = 0;
+                for &byte in line {
+                    if byte == b' ' {
+                        break;
+                    } else {
+                        file_size = file_size * 10 + (byte - b'0') as u32;
+                    }
+                }
+                directories[*path.last().unwrap()] += file_size;
+            }
+        }
+    }
+    while let Some(index) = path.pop() {
+        if let Some(&last) = path.last() {
+            directories[last] += directories[index];
         }
     }
     let needed_space = directories.get(&root).unwrap() - 40000000;
@@ -87,5 +85,5 @@ pub fn p2(input: Vec<u8>) {
             .filter(|&&size| size >= needed_space)
             .min()
             .unwrap()
-    )
+    );
 }
