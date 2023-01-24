0b923eef78ac5e97c11195e0eb96e3104571e083
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-24 03:10:37 +0100

    Optimize d08b from 484100us down to 315145us.

--- src/solutions/d08.rs
+++ src/solutions/d08.rs
@@ -36,33 +36,46 @@ pub fn p1(input: Vec<u8>) {
     rprintln!("d08a: {}", visible);
 }
 
+/// Measured speed: 315145us.
 pub fn p2(input: Vec<u8>) {
     let width = input.iter().position(|&d| d == b'\n').unwrap();
     let height = input.iter().rposition(|&d| d == b'\n').unwrap() / width;
 
     let mut best_score = 0;
-    for x in 0..width {
-        for y in 0..height {
-            best_score = best_score.max(
-                (0..x)
-                    .rev()
-                    .position(|nx| input[y * (width + 1) + nx] >= input[y * (width + 1) + x])
-                    .unwrap_or_else(|| x.wrapping_sub(1))
-                    .wrapping_add(1)
-                    * (x + 1..width)
-                        .position(|nx| input[y * (width + 1) + nx] >= input[y * (width + 1) + x])
-                        .unwrap_or_else(|| (width - x - 1).wrapping_sub(1))
-                        .wrapping_add(1)
-                    * (0..y)
-                        .rev()
-                        .position(|ny| input[ny * (width + 1) + x] >= input[y * (width + 1) + x])
-                        .unwrap_or_else(|| y.wrapping_sub(1))
-                        .wrapping_add(1)
-                    * (y + 1..height)
-                        .position(|ny| input[ny * (width + 1) + x] >= input[y * (width + 1) + x])
-                        .unwrap_or_else(|| (height - y - 1).wrapping_sub(1))
-                        .wrapping_add(1),
-            );
+    for x in 1..width - 1 {
+        for y in 1..height - 1 {
+            let tree = input[y * (width + 1) + x];
+            let mut left = x;
+            let mut right = width - 1 - x;
+            let mut top = y;
+            let mut bottom = height - 1 - y;
+
+            for (pos, nx) in (0..x).rev().enumerate() {
+                if input[y * (width + 1) + nx] >= tree {
+                    left = pos + 1;
+                    break;
+                }
+            }
+            for (pos, nx) in (x + 1..width).enumerate() {
+                if input[y * (width + 1) + nx] >= tree {
+                    right = pos + 1;
+                    break;
+                }
+            }
+            for (pos, ny) in (0..y).rev().enumerate() {
+                if input[ny * (width + 1) + x] >= tree {
+                    top = pos + 1;
+                    break;
+                }
+            }
+            for (pos, ny) in (y + 1..height).enumerate() {
+                if input[ny * (width + 1) + x] >= tree {
+                    bottom = pos + 1;
+                    break;
+                }
+            }
+
+            best_score = best_score.max(left * right * top * bottom);
         }
     }
 
