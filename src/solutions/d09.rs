ca743a665427213147070bdb208f4d7fc2cc65ee
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-31 00:46:02 +0100

    Speedup solution for d09a. Could still be significantly faster I think.

--- src/solutions/d09.rs
+++ src/solutions/d09.rs
@@ -11,12 +11,14 @@ const BL: u8 = 0b00000100;
 const BM: u8 = 0b00000010;
 const BR: u8 = 0b00000001;
 
-/// Measured speed: 82,059,566us.
-pub fn p1(input: Vec<u8>) {
+/// Measured speed: 46,193,655us.
+pub fn p1(memory: &mut [u8], input: &[u8]) {
     let mut rope: Vec<(i16, i16)> = vec![(0, 0); 2];
-    let mut tail_motions = Vec::with_capacity(12000);
+    let mut tail_motions =
+        unsafe { Vec::from_raw_parts(memory.as_mut_ptr().add(input.len()), 0, 10000) };
     let mut direction = 0;
     let mut steps = 0;
+
     for byte in input.into_iter() {
         match byte {
             b'U' => direction = TM,
@@ -49,15 +51,16 @@ pub fn p1(input: Vec<u8>) {
                         rope[tail].0 += x_diff.signum();
                         rope[tail].1 += y_diff.signum();
                         match (x_diff.signum(), y_diff.signum()) {
-                            (-1, 1) => tail_motions.push(TL),
-                            (0, 1) => tail_motions.push(TM),
-                            (1, 1) => tail_motions.push(TR),
-                            (-1, 0) => tail_motions.push(ML),
-                            (1, 0) => tail_motions.push(MR),
-                            (-1, -1) => tail_motions.push(BL),
-                            (0, -1) => tail_motions.push(BM),
-                            (_, _) => tail_motions.push(BR),
+                            (-1, 1) => tail_motions.push_within_capacity(TL),
+                            (0, 1) => tail_motions.push_within_capacity(TM),
+                            (1, 1) => tail_motions.push_within_capacity(TR),
+                            (-1, 0) => tail_motions.push_within_capacity(ML),
+                            (1, 0) => tail_motions.push_within_capacity(MR),
+                            (-1, -1) => tail_motions.push_within_capacity(BL),
+                            (0, -1) => tail_motions.push_within_capacity(BM),
+                            (_, _) => tail_motions.push_within_capacity(BR),
                         }
+                        .unwrap();
                     }
                 }
                 steps = 0;
@@ -66,9 +69,27 @@ pub fn p1(input: Vec<u8>) {
             _ => steps = steps * 10 + (byte - b'0'),
         }
     }
+    let mut new_motions = unsafe { Vec::from_raw_parts(memory.as_mut_ptr(), 0, 10000) };
+    for &motion in &tail_motions {
+        new_motions.push_within_capacity(motion).unwrap();
+    }
+    tail_motions.into_raw_parts();
+    let mut tail_motions = new_motions;
+    let spare_space = (memory.len() - tail_motions.capacity()) / 4;
+    let mut tail_positions = unsafe {
+        Vec::from_raw_parts(
+            tail_motions
+                .as_mut_ptr()
+                .add(tail_motions.capacity() + 4 - tail_motions.capacity() % 4)
+                .cast::<(i16, i16)>(),
+            0,
+            spare_space,
+        )
+    };
+
     let mut visited = 1;
     let (mut x, mut y) = (0, 0);
-    'outer: for i in 0..tail_motions.len() {
+    for i in 0..tail_motions.len() {
         match tail_motions[i] {
             TL => (x, y) = (x - 1, y + 1),
             TM => (x, y) = (x, y + 1),
@@ -79,25 +100,36 @@ pub fn p1(input: Vec<u8>) {
             BM => (x, y) = (x, y - 1),
             _ => (x, y) = (x + 1, y - 1),
         }
-        let (mut nx, mut ny) = (0, 0);
-        for j in 0..i {
-            match tail_motions[j] {
-                TL => (nx, ny) = (nx - 1, ny + 1),
-                TM => (nx, ny) = (nx, ny + 1),
-                TR => (nx, ny) = (nx + 1, ny + 1),
-                ML => (nx, ny) = (nx - 1, ny),
-                MR => (nx, ny) = (nx + 1, ny),
-                BL => (nx, ny) = (nx - 1, ny - 1),
-                BM => (nx, ny) = (nx, ny - 1),
-                _ => (nx, ny) = (nx + 1, ny - 1),
+        if i < spare_space {
+            if !tail_positions.contains(&(x, y)) {
+                visited += 1;
             }
-            if (x, y) == (nx, ny) {
-                continue 'outer;
+            tail_positions.push_within_capacity((x, y)).unwrap();
+        } else {
+            let (mut nx, mut ny) = tail_positions.last().unwrap();
+            if !tail_positions.contains(&(x, y))
+                && (spare_space..i).all(|j| {
+                    match tail_motions[j] {
+                        TL => (nx, ny) = (nx - 1, ny + 1),
+                        TM => (nx, ny) = (nx, ny + 1),
+                        TR => (nx, ny) = (nx + 1, ny + 1),
+                        ML => (nx, ny) = (nx - 1, ny),
+                        MR => (nx, ny) = (nx + 1, ny),
+                        BL => (nx, ny) = (nx - 1, ny - 1),
+                        BM => (nx, ny) = (nx, ny - 1),
+                        _ => (nx, ny) = (nx + 1, ny - 1),
+                    };
+                    (x, y) != (nx, ny)
+                })
+            {
+                visited += 1;
             }
         }
-        visited += 1;
     }
     rprintln!("d09a: {}", visited);
+
+    tail_motions.into_raw_parts();
+    tail_positions.into_raw_parts();
 }
 
 pub fn p2(_memory: &mut [u8], _input: &[u8]) {
