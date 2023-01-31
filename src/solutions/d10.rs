89888dff97ee3359d25538bfe13fd914bad834d3
Vlamonster <jellemartijndijkstra@gmail.com>
2023-01-31 02:26:08 +0100

    Solution for d10a.

--- src/solutions/d10.rs
+++ src/solutions/d10.rs
@@ -1,7 +1,61 @@
+use alloc::vec::Vec;
 use rtt_target::rprintln;
 
-pub fn p1(_memory: &mut [u8], _input: &[u8]) {
-    rprintln!("d10a: {}", "nyi");
+pub fn p1(memory: &mut [u8], input: &[u8]) {
+    let mut trace = unsafe {
+        Vec::from_raw_parts(
+            memory
+                .as_mut_ptr()
+                .add(input.len() + 4 - input.len() % 4)
+                .cast::<i32>(),
+            0,
+            512,
+        )
+    };
+
+    trace.push(0);
+
+    let mut i = 0;
+    let mut sign = 1;
+    let mut operand = 0;
+    let mut x = 1;
+    while i < input.len() {
+        match input[i] {
+            b'n' => {
+                i += 5;
+                trace.push_within_capacity(x).unwrap();
+            }
+            b'a' => i += 5,
+            b'-' => {
+                sign = -1;
+                i += 1;
+            }
+            b'\n' => {
+                trace.push_within_capacity(x).unwrap();
+                trace.push_within_capacity(x).unwrap();
+
+                x += sign * operand;
+
+                i += 1;
+                sign = 1;
+                operand = 0;
+            }
+            _ => {
+                operand = operand * 10 + (input[i] - b'0') as i32;
+                i += 1;
+            }
+        }
+    }
+
+    rprintln!(
+        "d10a: {}",
+        [20, 60, 100, 140, 180, 220]
+            .iter()
+            .map(|&cycle| trace[cycle] * cycle as i32)
+            .sum::<i32>()
+    );
+
+    trace.into_raw_parts();
 }
 
 pub fn p2(_memory: &mut [u8], _input: &[u8]) {
