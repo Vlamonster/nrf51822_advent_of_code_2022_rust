e6c922c312323e1a1b6f106a5484a70876eddd21
Vlamonster <jellemartijndijkstra@gmail.com>
2023-02-02 15:53:05 +0100

    Speedup for d03.

--- src/solutions/d03.rs
+++ src/solutions/d03.rs
@@ -1,13 +1,9 @@
 use rtt_target::rprintln;
 
-/// Measured speed: 29,119us.
+/// Measured speed: 22,228us.
 pub fn p1(_memory: &mut [u8], input: &mut [u8]) {
     let mut total = 0;
     for backpack in input.split(|&byte| byte == b'\n') {
-        // Split the backpack string into the first and second half
-        let first_half = &backpack[0..backpack.len() / 2];
-        let second_half = &backpack[backpack.len() / 2..backpack.len()];
-
         // Create variables to store the available lowercase and uppercase letters in each half
         let mut first_half_lowercase = 0u32;
         let mut first_half_uppercase = 0u32;
@@ -15,22 +11,24 @@ pub fn p1(_memory: &mut [u8], input: &mut [u8]) {
         let mut second_half_uppercase = 0u32;
 
         // Iterate through each character in the first half and mark it as available
-        for &item in first_half {
+        backpack[0..backpack.len() / 2].iter().for_each(|&item| {
             if item >= b'a' {
                 first_half_lowercase |= 2 << (item - b'a');
             } else {
                 first_half_uppercase |= 2 << (item - b'A');
             }
-        }
+        });
 
         // Iterate through each character in the second half and mark it as available
-        for &item in second_half {
-            if item >= b'a' {
-                second_half_lowercase |= 2 << (item - b'a');
-            } else {
-                second_half_uppercase |= 2 << (item - b'A');
-            }
-        }
+        backpack[backpack.len() / 2..backpack.len()]
+            .iter()
+            .for_each(|&item| {
+                if item >= b'a' {
+                    second_half_lowercase |= 2 << (item - b'a');
+                } else {
+                    second_half_uppercase |= 2 << (item - b'A');
+                }
+            });
 
         if second_half_lowercase & first_half_lowercase != 0 {
             total += (second_half_lowercase & first_half_lowercase).trailing_zeros();
@@ -41,7 +39,7 @@ pub fn p1(_memory: &mut [u8], input: &mut [u8]) {
     rprintln!("d03a: {}", total);
 }
 
-/// Measured speed: 29,016us.
+/// Measured speed: 21,721us.
 pub fn p2(_memory: &mut [u8], input: &mut [u8]) {
     let mut total = 0;
     for [elf1, elf2, elf3] in input.split(|&byte| byte == b'\n').array_chunks() {
@@ -52,29 +50,29 @@ pub fn p2(_memory: &mut [u8], input: &mut [u8]) {
         let mut elf3_lowercase = 0u32;
         let mut elf3_uppercase = 0u32;
 
-        for &item in elf1 {
+        elf1.iter().for_each(|&item| {
             if item >= b'a' {
                 elf1_lowercase |= 2 << (item - b'a');
             } else {
                 elf1_uppercase |= 2 << (item - b'A');
             }
-        }
+        });
 
-        for &item in elf2 {
+        elf2.iter().for_each(|&item| {
             if item >= b'a' {
                 elf2_lowercase |= 2 << (item - b'a');
             } else {
                 elf2_uppercase |= 2 << (item - b'A');
             }
-        }
+        });
 
-        for &item in elf3 {
+        elf3.iter().for_each(|&item| {
             if item >= b'a' {
                 elf3_lowercase |= 2 << (item - b'a');
             } else {
                 elf3_uppercase |= 2 << (item - b'A');
             }
-        }
+        });
 
         if elf1_lowercase & elf2_lowercase & elf3_lowercase != 0 {
             total += (elf1_lowercase & elf2_lowercase & elf3_lowercase).trailing_zeros();
